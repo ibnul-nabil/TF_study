364ad3ae50c76227357cf511e279045420fb0740
ibnul-nabil <2005060@ugrad.cse.buet.ac.bd>
2023-05-22 18:05:47 +0600

    version 2

--- new.cpp
+++ new.cpp
@@ -5,5 +5,10 @@ int main(){
     string s;
     cin >> s ; 
     cout << s << " ekta vodai\n";
+
+
+    int a;
+    cin >> a;
+    cout << s << " er roll " << a;
     return 0;
 }
---
+++ notun.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector <int> a;
+
+    for (int i=0;i<5;i++) {
+        int x;
+        cin >> x;
+        a.push_back(x);
+
+    }
+
+    sort(a.begin(),a.end());
+
+    return 0;
+}
