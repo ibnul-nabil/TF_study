db92669e91ddf25d3c5482370bc93d62ff293d1a
ibnul-nabil <2005060@ugrad.cse.buet.ac.bd>
2023-05-22 18:32:49 +0600

    version 1 updated

--- new.cpp
+++ new.cpp
@@ -5,13 +5,5 @@ int main(){
     string s;
     cin >> s ; 
     cout << s << " ekta vodai\n";
-
-
-    int a;
-    cin >> a;
-    cout << s << " er roll " << a;
-
-    // in version 3 
-    cout << " in version 3 ";
     return 0;
 }
